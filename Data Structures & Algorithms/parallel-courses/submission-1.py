6d8eeb3bdb624ca97ec6347543d5030112ff2775
Justin Ventura <55901784+jventura1738@users.noreply.github.com>
2026-04-24 16:07:21 -0700

    Add: parallel-courses - submission-1

---
+++ Data Structures & Algorithms/parallel-courses/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        # n+1 since we do 1 indexing for whatever dumbass reason
+        adj = [[] for _ in range(n+1)]
+        indegrees = [0] * (n+1)
+        visited = set()
+        for prv, nxt in relations:
+            adj[prv].append(nxt)
+            indegrees[nxt] += 1
+        
+        queue = deque()
+        
+        for i, indegree in enumerate(indegrees[1:]):
+            if indegree == 0:
+                queue.append(i+1)
+
+        time = 0
+        while queue:
+            
+            qlen = len(queue)
+            for _ in range(qlen):
+                curr = queue.popleft()
+                visited.add(curr)
+
+                for neighbor in adj[curr]:
+                    if neighbor in visited:
+                        continue
+                    indegrees[neighbor] -= 1
+                    if indegrees[neighbor] == 0:
+                        queue.append(neighbor)
+            time += 1
+        
+        return time if len(visited) == n else -1
+        
